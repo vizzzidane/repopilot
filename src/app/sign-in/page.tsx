6f8997564f4f7c14d2ed09965e70487e59272cf6
vizzzidane <visshwa2310@gmail.com>
2026-05-13 01:57:42 +0800

    Fix Clerk sign-in redirect prop

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-black px-6">
-      <SignIn routing="path" path="/sign-in" afterSignInUrl="/" />
+      <SignIn routing="path" path="/sign-in" fallbackRedirectUrl="/" />
     </main>
   );
 }
