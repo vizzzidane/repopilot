49ec4631d821ce5dbcb27fcd0c6d0aee5e120dac
vizzzidane <visshwa2310@gmail.com>
2026-05-14 00:28:30 +0800

    Configure Auth.js production settings

--- auth.ts
+++ auth.ts
@@ -2,7 +2,16 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  trustHost: true,
+  secret: process.env.AUTH_SECRET,
+
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
+
   callbacks: {
     session({ session, token }) {
       if (session.user) {
